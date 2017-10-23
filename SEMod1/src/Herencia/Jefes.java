4ef0d4cbdceddabd0a322cdaffbe048df467e886
samuel sanchez <shamelo9510@gmail.com>
2017-10-22 23:20:01 -0500

    herencia II

---
+++ SEMod1/src/Herencia/Jefes.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Herencia;
+
+/**
+ *
+ * @author developer
+ */
+public class Jefes extends Empleado
+{
+   
+    public  Jefes(String nom,double sueldo , int ano,int mes ,int dia)
+    {
+        super(nom,sueldo,ano,mes,dia);
+    }
+    
+    public void estableceIncetivo(double b)
+    {
+        incentivo = b;
+    }
+    public double dameSueldo()
+    {
+        double sueldoJefe=super.dameSueldo();
+        return incentivo+sueldoJefe;
+    }
+    private double incentivo;
+    
+    
+    
+}
